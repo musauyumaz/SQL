a8c21a9d41f2dc8a640b0c1c76801b52fbcd441e
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-06-27 22:01:10 +0300

    4-LEFT RIGHT Fonksiyonları

---
+++ 1-String Fonksiyonlar/4-LEFT RIGHT Fonksiyonları.sql
@@ -0,0 +1,5 @@
+-- LEFT RIGHT Fonksiyonlarý
+-- Saðdan ve soldan belli sayýda karakteri almak için kullandýðýmýz STRÝNG fonksiyonlarýdýr.
+
+SELECT LEFT('BTK AKADEMÝ',3)
+SELECT RIGHT('BTK AKADEMÝ',7)
