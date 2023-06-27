de1e47b4588e08ada1d142a0ecb59618455cea22
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-06-27 22:17:43 +0300

    7-REPLACE Fonksiyonu

---
+++ 1-String Fonksiyonlar/7-REPLACE Fonksiyonu.sql
@@ -0,0 +1,3 @@
+-- REPLACE Fonksiyonu
+-- Bir STRÝNG ifadenin içerisinde istediðimiz harf(ler)i deðiþtirebiliriz. 
+SELECT REPLACE('T-SQL ile herhangi bir programlama dili ile yapýlabilecek hemen hemen herþeyi yapabilirsiniz.','T-SQL','TRANSACT SQL')
