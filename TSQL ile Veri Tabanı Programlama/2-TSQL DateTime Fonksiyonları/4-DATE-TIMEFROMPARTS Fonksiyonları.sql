a5620a5c7135d176d85f83e5a5e6a9fe8a913400
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-06-29 12:33:01 +0300

    4-DATE-TIMEFROMPARTS Fonksiyonları

---
+++ TSQL ile Veri Tabanı Programlama/2-TSQL DateTime Fonksiyonları/4-DATE-TIMEFROMPARTS Fonksiyonları.sql
@@ -0,0 +1,5 @@
+-- 4-DATE-TIMEFROMPARTS Fonksiyonlarý
+-- Ýçine gönderdiðimiz parametrelerden fonksiyona göre sonuç döndüren fonksiyonlardýr.
+SELECT DATEFROMPARTS(2023,6,29)
+SELECT DATETIMEFROMPARTS(2023,6,29,12,30,10,415)
+SELECT TIMEFROMPARTS(12,30,10,415,3)
