6f5be686c99d1d8e1fb98ab5811e6ca6e0951dec
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-07-01 00:06:17 +0300

    5-Tip Dönüştürme Fonksiyonları Alıştırma 4

---
+++ TSQL ile Veri Tabanı Programlama/3-Tip Dönüştürme Fonksiyonları/5-Tip Dönüştürme Fonksiyonları Alıştırma 4.sql
@@ -0,0 +1,13 @@
+-- 5-Tip Dönüþtürme Fonksiyonlarý Alýþtýrma 4
+-- Elimizde bir ürünler listesi var. Bu ürün listesi için tekil bir akýllý barkod oluþturmamýz gerekiyor. Ýçine ürünkodu alan ve bu koda göre akýllý barkod üreten T-SQL kodunu yazýnýz.
+-- Barkod Formatý:
+-- Categorycode-ITEMCODE-YEAR-MONTH-DAY-HOUR-MINUTE-SECOND
+-- Örnek:GDA-000025-2022-06-15-12-24-37
+-- Kaynak Tablo: LAB03
+SELECT * FROM LAB03
+
+SELECT CATEGORY1CODE+'-'+REPLICATE('0',6-LEN(ITEMCODE)) + ITEMCODE + '-' + REPLACE(CONVERT(VARCHAR,GETDATE(),102),'.','-') + '-' +  REPLACE(CONVERT(VARCHAR,GETDATE(),108),':','-') FROM LAB03
+
+SELECT CONVERT(VARCHAR,GETDATE(),108)
+
+SELECT CATEGORY1CODE + REPLICATE('0',6-LEN(ITEMCODE)) + ITEMCODE + REPLACE(CONVERT(VARCHAR,GETDATE(),102),'.','') +  REPLACE(CONVERT(VARCHAR,GETDATE(),108),':','') FROM LAB03
