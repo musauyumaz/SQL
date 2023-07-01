23c51f3dce28d63ed0eead192dca62589315e754
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-07-01 10:52:56 +0300

    1- ABS/SIGN Fonksiyonları

---
+++ TSQL ile Veri Tabanı Programlama/4-Matematik Fonksiyonları/1- ABS SIGN Fonksiyonları.sql
@@ -0,0 +1,6 @@
+-- 1- ABS/SIGN Fonksiyonlarý
+-- ABS(ABSOLUTE) : Mutlak deðer fonksiyonu
+-- SIGN : Ýþaret fonksiyonu. Bir sayýnýn iþaretini bize getirir. Ýçine aldýðý deðer pozitifse 1 negatifse -1 deðerini getirir.
+SELECT ABS(-35.7),SIGN(-24.5)
+
+SELECT ABS(25),ABS(-25),ABS(-25.56),SIGN(25), SIGN(-25),SIGN(-25.56),SIGN(0)
